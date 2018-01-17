59ab7085a7aec5ddbae0a55275827e35070ea9ad
Eduardo091 <urrutia.e14@gmail.com>
2018-01-16 19:22:02 -0600

    added background fixed bug

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,11 +4,6 @@ Dancer.call(this, top, left, timeBetweenSteps);
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-
-
-
-
-
 BlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this);
